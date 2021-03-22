91ee9f9b09769fa305bc29ffb210cbbae0ce8319
Abdrashov <abdrashovv@gmail.com>
2021-03-22 22:41:41 +0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class, 'user_id');
+    }
 }
