4ace1e246d9c3e211ed1c44176534b053b898f81
Abdrashov <abdrashovv@gmail.com>
2021-03-22 22:41:48 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                             
                             <!-- Logo -->
                             <div class="flex-shrink-0 flex items-center">
-                                <a href="{{ route('dashboard') }}">
+                                <a href="{{ route('lesson.index') }}">
                                     <x-jet-application-mark class="block h-9 w-auto" />
                                 </a>
                             </div>
