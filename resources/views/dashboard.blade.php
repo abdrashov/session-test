32cc18643ab348304f16825a491503b6f5e46622
Abdrashov <abdrashovv@gmail.com>
2021-03-26 21:10:09 +0600

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,11 @@
 
   <div class="py-12"> 
   	<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      
+
+
+      
+
       <div class="overflow-hidden sm:rounded-lg">
         <div class="flex flex-col">
           <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
