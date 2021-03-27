d46e8740ac1a8213f47e719777bc3212b7dd2cdf
Abdrashov <abdrashovv@gmail.com>
2021-03-27 23:54:10 +0600

    Update user-test.blade.php

--- resources/views/livewire/user/user-test.blade.php
+++ resources/views/livewire/user/user-test.blade.php
@@ -1,7 +1,7 @@
 
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Dashboard') }}
+      {{ __('Тесты') }}
     </h2>
   </x-slot>
 
@@ -85,40 +85,31 @@
     </div>
   </div>
 
-<div class="mt-5">
-	<x-jet-danger-button wire:click="confirmUserDeletion" wire:loading.attr="disabled">
-	    {{ __('Delete Account') }}
-	</x-jet-danger-button>
-</div>
+  <div class="mt-5">
+      <x-jet-danger-button wire:click="confirmUserDeletion" wire:loading.attr="disabled">
+          {{ __('Delete Account') }}
+      </x-jet-danger-button>
+  </div>
 
-<!-- Delete User Confirmation Modal -->
-<x-jet-dialog-modal wire:model="confirmingUserDeletion">
-	<x-slot name="title">
-	    {{ __('Delete Account') }}
-	</x-slot>
+  <!-- Delete User Confirmation Modal -->
+ <x-jet-confirmation-modal wire:model="confirmingUserDeletion">
+    <x-slot name="title">
+        Delete Account
+    </x-slot>
 
-	<x-slot name="content">
-	    {{ __('Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+    <x-slot name="content">
+        Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted.
+    </x-slot>
 
-	    <div class="mt-4" x-data="{}" x-on:confirming-delete-user.window="setTimeout(() => $refs.password.focus(), 250)">
-	        <x-jet-input type="password" class="mt-1 block w-3/4"
-	                    placeholder="{{ __('Password') }}"
-	                    x-ref="password"
-	                    wire:model.defer="password"
-	                    wire:keydown.enter="deleteUser" />
+    <x-slot name="footer">
+        <x-jet-secondary-button wire:click="$toggle('confirmingUserDeletion')" wire:loading.attr="disabled">
+            Nevermind
+        </x-jet-secondary-button>
 
-	        <x-jet-input-error for="password" class="mt-2" />
-	    </div>
-	</x-slot>
+        <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
+            Delete Account
+        </x-jet-danger-button>
+    </x-slot>
+</x-jet-confirmation-modal>
 
-	<x-slot name="footer">
-	    <x-jet-secondary-button wire:click="$toggle('confirmingUserDeletion')" wire:loading.attr="disabled">
-	        {{ __('Cancel') }}
-	    </x-jet-secondary-button>
-
-	    <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
-	        {{ __('Delete Account') }}
-	    </x-jet-danger-button>
-	</x-slot>
-</x-jet-dialog-modal>
 
