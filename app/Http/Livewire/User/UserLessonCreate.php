2962ef419daf2ae052817d44954cacf69ff2666f
Abdrashov <abdrashovv@gmail.com>
2021-03-29 23:47:26 +0600

    Update UserLessonCreate.php

--- app/Http/Livewire/User/UserLessonCreate.php
+++ app/Http/Livewire/User/UserLessonCreate.php
@@ -2,29 +2,43 @@
 
 namespace App\Http\Livewire\User;
 
-use App\Models\Question;
+use App\Models\Lesson;
 use Livewire\Component;
 
 class UserLessonCreate extends Component
 {
+  public $lesson;
 	public $questions;
-
-	public $answers;
+  public $answers;
+  private $lesson_id;
 
   protected $rules = [
     'questions.title' => 'required|string',
-    'answers.*.title' => 'required|string',
+    'answers.0.title' => 'required|string',
+    'answers.1.title' => 'required|string',
+    'answers.2.title' => 'required|string',
+    'answers.3.title' => 'required|string',
+    'answers.4.title' => 'required|string',
   ];
+
+  public function mount($id)
+  {
+    $this->lesson_id = $id;
+  }
 
   public function render()
   {
+    $this->lesson = auth()->user()->lessons()->with('questions.answers')->find($this->lesson_id);
     return view('livewire.user.user-lesson-create');
   }
 
   public function save()
   {
   	$this->validate();
-  	dd(Question::create($this->questions)->answers()->createMany($this->answers));
+    $params = $this->answers;
+    $params[0] = array_merge($params[0], ['status' => true]);
+  	$this->lesson->questions()->create($this->questions)->answers()->createMany($params);
+    unset($this->answers, $this->questions);
   }
 
 
