0be6267b4ee818b7e06124ab82d4536f83038dc1
Abdrashov <abdrashovv@gmail.com>
2021-03-30 22:29:17 +0600

    Update UserTest.php

--- app/Http/Livewire/User/UserTest.php
+++ app/Http/Livewire/User/UserTest.php
@@ -6,22 +6,9 @@ use Livewire\Component;
 
 class UserTest extends Component
 {
-  public $model;
-
-  protected $listeners = ['confirmingUserDeletion' => 'model'];
-
-  public function model()
-  {
-  	$this->model = true;
-  }
-
-  public function mount()
-  {
-  	$this->emit('userDelete');
-  }
-
   public function render()
   {
-    return view('livewire.user.user-test');
+    $ratings = auth()->user()->ratings()->paginate(15);
+    return view('livewire.user.user-test', compact('ratings'));
   }
 }
