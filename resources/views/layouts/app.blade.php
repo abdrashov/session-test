11be987b505b3d7611d32fc312b319f3fbac60e7
Abdrashov <abdrashovv@gmail.com>
2021-04-08 00:19:32 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,73 +1,84 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+	<title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+	<!-- Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+	<!-- Styles -->
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        @livewireStyles
+	@livewireStyles
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+	<!-- Scripts -->
+	<script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body class="font-sans antialiased">
+	<x-jet-banner />
 
-            <div wire:offline.class="absolute top-0 w-full">
-                <div wire:offline wire:offline.class="bg-red-100 text-red-900 rounded-lg shadow-md p-4 m-4">
-                  Ой! Произошло что-то ужасное. Проверьте ваше интернет-соединение!
-                </div>
-            </div>
+	<div wire:offline.class="absolute top-0 w-full">
+		<div wire:offline wire:offline.class="bg-red-100 text-red-900 rounded-lg shadow-md p-4 m-4">
+			Ой! Произошло что-то ужасное. Проверьте ваше интернет-соединение!
+		</div>
+	</div>
 
-        <div class="min-h-screen bg-gray-100">
-            @auth
-              @livewire('navigation-menu')
-            @else
-                <nav class="bg-white border-b border-gray-100">
-                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                        <div class="flex justify-between h-16">
-                            
-                            <!-- Logo -->
-                            <div class="flex-shrink-0 flex items-center">
-                                <a href="{{ route('lesson.index') }}">
-                                    <x-jet-application-mark class="block h-9 w-auto" />
-                                </a>
-                            </div>
+	<div class="min-h-screen bg-gray-100">
+		@auth
+			@livewire('navigation-menu')
+		@else
+		<nav class="bg-white border-b border-gray-100">
+			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div class="flex justify-between h-16">
 
-                            <div class="flex">
+					<!-- Logo -->
+					<div class="flex-shrink-0 flex items-center">
+						<a href="{{ route('lesson.index') }}">
+							<x-jet-application-mark class="block h-9 w-auto" />
+						</a>
+					</div>
 
-                                <!-- Navigation Links -->
-                                <div class="-my-px ml-10 flex">
-                                    <x-jet-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
-                                        {{ __('Login') }}
-                                    </x-jet-nav-link>
-                                </div>
+					<div class="flex">
 
-                                <div class="-my-px ml-10 flex">
-                                    <x-jet-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
-                                        {{ __('Register') }}
-                                    </x-jet-nav-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            @endauth
+						<!-- Navigation Links -->
+						<div class="-my-px ml-10 flex">
+							<x-jet-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
+								{{ __('Login') }}
+							</x-jet-nav-link>
+						</div>
 
-            {{ $slot }}
+						<div class="-my-px ml-10 flex">
+							<x-jet-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
+								{{ __('Register') }}
+							</x-jet-nav-link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		@endauth
 
-        </div>
+		<!-- Page Heading -->
+		@if (isset($header))
+			<header class="bg-white shadow">
+				<div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+					{{ $header }}
+				</div>
+			</header>
+		@endif
 
-        @stack('modals')
+		<main>
+			{{ $slot }}
+		</main>
 
-        @livewireScripts
-    </body>
+	</div>
+
+	@stack('modals')
+
+	@livewireScripts
+</body>
 </html>
