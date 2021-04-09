ed46a4fcf0464ca8c222feddb488ce1791ae371d
Abdrashov <abdrashovv@gmail.com>
2021-04-09 19:22:00 +0600

    Update TestResult.php

--- app/Http/Livewire/User/TestResult.php
+++ app/Http/Livewire/User/TestResult.php
@@ -8,13 +8,15 @@ use Livewire\Component;
 class TestResult extends Component
 {
 	public $rating;
+	public $no_response;
 
 	public function mount($code){
 		$this->rating = auth()->user()->ratings()->where('code', $code)->with('tests.answer1', 'tests.answer2', 'tests.answer3', 'tests.answer4', 'tests.answer5', 'tests.rightAnswerId')->first();
 	}
 
-  public function render()
-  {
-    return view('livewire.user.test-result');
-  }
+	public function render()
+	{
+		$this->no_response = $this->rating->tests()->where('user_answer_id', 0)->where('spent_time', 0)->count();
+		return view('livewire.user.test-result');
+	}
 }
