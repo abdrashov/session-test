5f79eb71ee8fcbeede8982046cb5bf232335af59
Abdrashov <abdrashovv@gmail.com>
2021-04-13 23:00:10 +0600

    Переместил шапку из корня в компоненты

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
 	<!-- Fonts -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -22,46 +22,13 @@
 <body class="font-sans antialiased">
 	<x-jet-banner />
 
-	<div wire:offline.class="absolute top-0 w-full">
-		<div wire:offline wire:offline.class="bg-red-100 text-red-900 rounded-lg shadow-md p-4 m-4">
-			Ой! Произошло что-то ужасное. Проверьте ваше интернет-соединение!
-		</div>
+	<div wire:offline>
+		Ой! Произошло что-то ужасное. Проверьте ваше интернет-соединение!
 	</div>
-
+	
 	<div class="min-h-screen bg-gray-100">
-		@auth
-			@livewire('navigation-menu')
-		@else
-		<nav class="bg-white border-b border-gray-100">
-			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-				<div class="flex justify-between h-16">
 
-					<!-- Logo -->
-					<div class="flex-shrink-0 flex items-center">
-						<a href="{{ route('lesson.index') }}">
-							<x-jet-application-mark class="block h-9 w-auto" />
-						</a>
-					</div>
-
-					<div class="flex">
-
-						<!-- Navigation Links -->
-						<div class="-my-px ml-10 flex">
-							<x-jet-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
-								{{ __('Login') }}
-							</x-jet-nav-link>
-						</div>
-
-						<div class="-my-px ml-10 flex">
-							<x-jet-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
-								{{ __('Register') }}
-							</x-jet-nav-link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
-		@endauth
+		<x-navigation-menu/>
 
 		<!-- Page Heading -->
 		@if (isset($header))
