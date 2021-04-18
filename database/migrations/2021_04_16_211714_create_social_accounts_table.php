33a6486ec8a107ac031c22f847cfbba789cb656d
Abdrashov <abdrashovv@gmail.com>
2021-04-18 18:41:33 +0600

    Рефакторинг бд auth_socialites в social_accounts

---
+++ database/migrations/2021_04_16_211714_create_social_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->bigInteger('provider_id');
+            $table->string('provider');
+            $table->text('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_accounts');
+    }
+}
